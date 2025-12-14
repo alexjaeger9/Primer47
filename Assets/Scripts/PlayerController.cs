4cacd84567bbb0c4beb40ced511123e4a51f7627
alexjaeger9 <Alexander.Jaeger@Student.HTW-Berlin.de>
2025-12-14 21:40:45 +0100

    Shooting TraceLine Visual (#5)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,6 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float gravity = -9.81f;
     [SerializeField] private float mouseSensitivity = 200f;
-
     private CharacterController controller;
     private Vector3 velocity;
     private Vector3 moveDirection;
@@ -45,8 +44,8 @@ public class PlayerController : MonoBehaviour
 
     void HandleMovementInput()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 inputDir = new Vector3(horizontal, 0f, vertical);
         if (inputDir.sqrMagnitude < 0.001f)
